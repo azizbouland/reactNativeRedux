458e8c80033d471194da31a2ffec5dbdf4942e71
aziz <aziz.bouland@gmail.com>
2020-12-12 17:43:57 +0300

    adds UI for counter and + and -

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useSelector } from 'react-redux';
 
 export default function App() {
+  const counter = useSelector(state => state.counter)
   return (
     <View style={styles.container}>
-      <Text>Text</Text>
-      <Text>Sth</Text>
+      <Text>Counter:</Text>
+      <Text>{counter}</Text>
+      <View style={{ flexWrap: 'wrap', width: 100, height: 10 }}>
+        <View style={{ width: 40, marginRight: 20 }}>
+          <Button
+            title="+"
+            width="20px">
+          </Button>
+        </View>
+        <View style={{ width: 40 }}>
+          <Button
+            title="-"
+            width="20px">
+          </Button>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
